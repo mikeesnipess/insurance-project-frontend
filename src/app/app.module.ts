f4dced40f06eed87b9924f44f9c987abca53ca5d
Artur Bejenari <artur.bejenari00@gmail.com>
2024-04-25 00:51:02 +0300

    Introduce all page from home to review, and add functional for that page, integration with stripe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,22 @@ import { FooterComponent } from './components/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header/header.component';
 import { DriverDetailsComponent } from './components/driver-details/driver-details/driver-details.component';
+import { SigningPageComponent } from './components/signing-page/signing-page/signing-page.component';
+import { PaymentPageComponent } from './components/payment-page/payment-page/payment-page.component';
+import { ReviewPageComponent } from './components/review-page/review-page/review-page.component';
+import { SignUpPageComponent } from './components/sign-up-page/sign-up-page/sign-up-page.component';
+import { PdfViewerComponent } from './components/pdf-viewer/pdf-viewer/pdf-viewer.component';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 const appRoutes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'company-details', component:CompanyDetailsComponent},
   {path: 'driver-details', component:DriverDetailsComponent},
+  {path: 'signing-page', component:SigningPageComponent},
+  {path: 'sign-up-page', component:SignUpPageComponent},
+  {path: 'payment-page', component:PaymentPageComponent},
+  {path: 'review-page', component:ReviewPageComponent},
+
 
 ]
 
@@ -26,7 +37,12 @@ const appRoutes: Routes = [
     CompanyDetailsComponent,
     FooterComponent,
     HeaderComponent,
-    DriverDetailsComponent
+    DriverDetailsComponent,
+    SigningPageComponent,
+    PaymentPageComponent,
+    ReviewPageComponent,
+    SignUpPageComponent,
+    PdfViewerComponent,
   ],
   imports: [
     BrowserModule,
@@ -34,6 +50,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes, { scrollPositionRestoration: 'enabled' }), // This will automatically reset the scroll position to the top on route change 
     BrowserAnimationsModule,
     NgSelectModule,
+    PdfViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
