b2cff9e4b4117cdb9e5966de2605f0a3f5be806a
Artur Bejenari <artur.bejenari00@gmail.com>
2024-05-24 16:03:00 +0300

    Receive the docuemnt for signing page from backend

--- src/app/components/signing-page/signing-page/signing-page.component.ts
+++ src/app/components/signing-page/signing-page/signing-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { StateService } from 'src/app/services/StateService/state.service'; // Import StateService
+import { StateService } from 'src/app/services/StateService/state.service';
+import { PdfService } from 'src/app/services/PdfService/pdf.service';
+import { DriverDetails } from 'src/app/models/carrier/driver-models/driver-details-model';
+import Usdot from 'src/app/services/USDOT/usdot';
 
 @Component({
   selector: 'app-signing-page',
@@ -8,16 +11,38 @@ import { StateService } from 'src/app/services/StateService/state.service'; // I
   styleUrls: ['./signing-page.component.css']
 })
 export class SigningPageComponent implements OnInit {
-  pdfSrc: string = 'assets/PdfSigning.pdf'; // Adjust the filename as necessary
+  pdfSrc: string = '';
   envelopeId: string | null = null;
 
-  constructor(private route: ActivatedRoute, private router: Router, private stateService: StateService) {} // Inject StateService
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private stateService: StateService,
+    private pdfService: PdfService
+  ) {}
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe(params => {
       this.envelopeId = params.get('envelopeId');
       console.log('Envelope ID:', this.envelopeId);
+      this.generateAndFetchPdf();
     });
+  }
+
+  generateAndFetchPdf(): void {
+    const dataTransferObject = this.stateService.getDataTransferObject();
+    if (dataTransferObject) {
+      const { driverDetails, companyDetails } = dataTransferObject;
+
+      this.pdfService.generateDocument(driverDetails, companyDetails).subscribe((pdfBlob: Blob) => {
+        const url = window.URL.createObjectURL(pdfBlob);
+        this.pdfSrc = url; // Set the pdfSrc to the generated PDF URL
+      }, error => {
+        console.error('Error generating PDF:', error);
+      });
+    } else {
+      console.error('No data available in StateService');
+    }
   }
 
   onSignClick(): void {
